2a3029ea46b056236195833587ca95a55c2e7167
Abirami-24 <58926857+Abirami-24@users.noreply.github.com>
2020-07-06 16:14:19 +0530

    Update importmodule.py

--- importmodule.py
+++ importmodule.py
@@ -3,31 +3,24 @@ import datetime
 import pip
 import pkg_resources
 built_in_mod = sys.modules
-#print(built_in_mod.keys())
-'''if "sys" in built_in_mod:
-    print("true")'''
-#a=[datetime]
-#res=set(a)-set(built_in_mod)
-#print(list(res))
-#print(built_in_mod)
+
+
 pkgs = []
 pkgs1=[]
-#print([pip.main(['list'])])
+
 installed = [pkg.key for pkg in pkg_resources.working_set]
-#print(installed)
+
 with open('app.py') as f:
     for lines in f:
         if "import" in lines:
             tmp = lines.split("import")
-            #print(tmp[-1])
-            if tmp[-1].strip() not in built_in_mod and tmp[-1].strip() not in installed:
-            #if tmp[-1].strip() not in built_in_mod:
-                    pkgs.append((tmp[-1].strip()))
-#print(pkgs)
-#pkgs = set(pkgs)-set(installed)
-#print(list(pkgs))
-for package in pkgs:
-    if package in installed or package.lower() in installed:
-            pkgs.remove(package)
-            print(pkgs)
+           
+            if tmp[-1].strip() not in built_in_mod and (tmp[-1].strip()).lower() not in installed:
+                pkgs.append((tmp[-1].strip()))
+if len(pkgs) is not 0:
+print(pkgs)
 pip.main(['install'] + pkgs)
+print("******************")
+else:
+print("All the Packages are installed...You are Good to go...")
+
