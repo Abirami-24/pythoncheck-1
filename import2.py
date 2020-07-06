ac0b5aa343b08923b33a0dadf177a43196ee0fe2
Abirami-24 <58926857+Abirami-24@users.noreply.github.com>
2020-07-06 18:08:02 +0530

    Update import2.py

--- import2.py
+++ import2.py
@@ -1,5 +1,6 @@
 import pip
 import importlib
+import pkg_resources
 def install_and_import(package):
     
     try:
@@ -8,11 +9,14 @@ def install_and_import(package):
         pip.main(['install', package])
     #finally:
        # globals()[package] = importlib.import_module(package)
-
-
+installed = [pkg.key for pkg in pkg_resources.working_set]
+builtin_modules = sys.modules
 with open('app.py') as f:
     for lines in f:
         if "import" in lines:
             tmp = lines.split("import")
             pack=tmp[-1].strip()
-            install_and_import(pack)  
+            if pack not in built_in_mod and pack.lower() not in installed:
+                install_and_import(pack)  
+            else:
+                print("installed lib")
